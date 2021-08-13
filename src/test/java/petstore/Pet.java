e4fa37b12d77427ed0d8937f840d6e622e245f06
natalipaula <natali.paula@sensedia.com>
2021-08-12 21:21:58 -0300

    Criei o teste para incluir o Pet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,19 +1,23 @@
+
 // 1 - Pacote
 package petstore;
+
 // 2 - Bibliotecas
 
-
 import org.testng.annotations.Test;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import static io.restassured.RestAssured.given;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 // 3 - Classe
 public class Pet {
     // 3.1 - Atributos
-    String uri = "https://petstore.swagger.io/v2/pet"; //endereço da entidade Pet
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
 
     // 3.2 - Métodos e Funções
     public String lerJson(String caminhoJson) throws IOException {
@@ -21,26 +25,49 @@ public class Pet {
     }
 
     // Incluir - Create - Post
-    @Test //Identifica o método ou função como um teste para o TestNG
+    @Test(priority = 1)  // Identifica o método ou função como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
+
         // Sintaxe Gherkin
         // Dado - Quando - Então
         // Given - When - Then
-        given()
+
+        given() // Dado
                 .contentType("application/json") // comum em API REST - antigas era "text/xml"
                 .log().all()
                 .body(jsonBody)
-                .when()
+                .when()  // Quando
                 .post(uri)
-                .then()
+                .then()  // Então
                 .log().all()
                 .statusCode(200)
+                .body("name", is("Visa"))
+                .body("status", is("available"))
+                .body("category.name", is("cachorro"))
+                .body("tags.name", contains("sta"))
         ;
+    }
+    @Test(priority = 2)
+    public void consultarPet(){
+        String petId = "1956022304";
+        String token =
 
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri+ "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Visa"))
+                .body("status", is("available"))
+                .body("category.name", is("cachorro"))
+                .body("tags.name", contains("sta"))
+        .extract()
+                .path("category.name");
+        System.out.println("O token é "+ token)
+              ;
     }
 }
-//cupon API40
-// CUPON REST49
-
-//ATÉ 18  DE AGOSTO
