14a4735bf32928246d0ef1ee6ef231af758ea2f6
natalipaula <natali.paula@sensedia.com>
2021-08-12 21:41:38 -0300

    Incluindo validaçao do metodo PUT

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -70,4 +70,22 @@ public class Pet {
         System.out.println("O token é "+ token)
               ;
     }
+
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+            .contentType("application/json")
+            .log().all()
+            .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("name", is("Vida"))
+            .body("status", is("sold"))
+        ;
+    }
 }
